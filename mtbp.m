0187690ee3a731451bd69f4dc09dc3762e8b2348
Ben Arthur <arthurb@janelia.hhmi.org>
2012-07-06 15:22:42 -0400

    only channels 1-4 processed, 5 (and higher) ignored

--- mtbp.m
+++ mtbp.m
@@ -66,6 +66,9 @@ df=f(2)-f(1);
 [p n e]=fileparts(FILEIN);
 DIR_OUT=fullfile(p);
 FILEINs=dir([FILEIN '.ch*']);
+[tmp{1:length(FILEINs)}]=deal(FILEINs.name);
+tmp=cellfun(@(x) regexp(x,'\.ch[1-4]'), tmp,'uniformoutput',false);
+FILEINs=FILEINs(~cellfun(@isempty,tmp));
 if(length(FILEINs)==0)
   error(['can''t find file ''' FILEIN '.ch*''']);
 end
