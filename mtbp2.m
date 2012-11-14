6dab82b66dc43c8973594cf902c8ccbba7d2fb2a
Ben Arthur <arthurb@janelia.hhmi.org>
2012-11-14 18:19:32 -0500

    save parameters used to .params

--- mtbp2.m
+++ mtbp2.m
@@ -564,6 +564,28 @@ if(GROUNDTRUTH)
   save([filename '.fa' sprintf('%d',CHANNELS)],'tmp','-ascii');
 end
 
+varname=@(x) inputname(1);
+fid=fopen([filename '.params' sprintf('%d',CHANNELS)],'w');
+for i=1:length(data)
+  jj=fieldnames(data(i));
+  for j=1:length(jj)-2
+    fprintf(fid,'%s=%g;\n',char(jj(j)),data(i).(char(jj(j))));
+  end
+  fprintf(fid,'\n');
+end
+fprintf(fid,'%s=%g;\n',varname(OBJ_SIZE),OBJ_SIZE);
+fprintf(fid,'%s=[%g %g];\n',varname(CONV_SIZE),CONV_SIZE);
+fprintf(fid,'%s=%g;\n',varname(F_LOW),F_LOW);
+fprintf(fid,'%s=%g;\n',varname(F_HIGH),F_HIGH);
+fprintf(fid,'%s=%g;\n',varname(NSEG),NSEG);
+fprintf(fid,'%s=[%g];\n',varname(MERGE_TIME),MERGE_TIME);
+fprintf(fid,'%s=%g;\n',varname(MERGE_FREQ),MERGE_FREQ);
+fprintf(fid,'%s=%g;\n',varname(MERGE_FREQ_OVERLAP),MERGE_FREQ_OVERLAP);
+fprintf(fid,'%s=%g;\n',varname(MERGE_FREQ_RATIO),MERGE_FREQ_RATIO);
+fprintf(fid,'%s=%g;\n',varname(MERGE_FREQ_FRACTION),MERGE_FREQ_FRACTION);
+fprintf(fid,'%s=[%s];\n',varname(CHANNELS),num2str(CHANNELS));
+fclose(fid);
+
 
 
 function mtbp2_print(i,left,right,type,filename,FS,NFFT,SAVE_WAV,SAVE_PNG,b,a,CHANNELS)
